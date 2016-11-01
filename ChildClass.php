2274fa688fd7a080760714ed1c8db403a5106da3
Jonathan Lobo <jpl68@pitt.edu>
2016-10-31 22:16:29 -0400

    First attempt at parent and child class (football teams)

--- ChildClass.php
+++ ChildClass.php
@@ -1,6 +1,53 @@
-<?php 
-	// this file will extend PArentClass.php
+<?php
+	// this file will extend ParentClass.php
 
 	class ChildClass extends ParentClass {
-		
+		private $city;
+    private $mascot;
+    private $wins;
+		private $losses;
+
+    public function __construct($c, $m, $w, $l) {
+      $this->city = $c;
+      $this->mascot = $m;
+      $this->wins = $w;
+			$this->losses = $l;
+    }
+
+    public function getOldCity() {
+      return parent::getName();
+    }
+
+    public function setCity($newCity) {
+      echo "<!-- Setting new City to " . $newCity . "-->";
+      $this->city = $newCity;
+    }
+
+    public function getOldMascot() {
+      return parent::getJob();
+    }
+
+    public function setMascot($newMascot) {
+      echo "<!-- Setting new Mascot to " . $newMascot . "-->";
+      $this->mascot = $newMascot;
+    }
+
+    public function getOldWins() {
+      return parent::getWins();
+    }
+
+    public function setMaritalStatus($newWins) {
+      echo "<!-- Setting new Wins to " . $newWins . "-->";
+      $this->wins = $newWins;
+    }
+
+		public function getOldLosses() {
+			return parent::getLosses();
+		}
+
+		public function setMaritalStatus($newLosses) {
+			echo "<!-- Setting new Losses to " . $newLosses . "-->";
+			$this->losses = $newLosses;
+		}
+
 	}
--- ParentClass.php
+++ ParentClass.php
@@ -2,5 +2,57 @@
 	// This is the file for the parent class
 
 	class ParentClass {
-		
+		private $city;
+    private $mascot;
+    private $wins;
+		private $losses;
+
+    public function __construct($c, $m, $w, $l) {
+      $this->city = $c;
+      $this->mascot = $m;
+      $this->wins = $w;
+			$this->losses = $l;
+    }
+
+    public function getCity() {
+      return $this->city;
+    }
+
+    public function getMascot() {
+      return $this->mascot;
+    }
+
+    public function getWins() {
+      return $this->wins;
+    }
+
+		public function getLosses() {
+      return $this->losses;
+    }
+
+    public function __toString() {
+			$totalGames = $wins + $losses;
+			$teamName = $this.getCity() . " " . $this.getMascot();
+			$record = $this.getWins() . " - " . $this.getLosses();
+
+			$status;
+
+			if ($totalGames < 16) {
+				if ($wins/($wins + $losses) >= .625) {
+					$status = "The season is looking promising!";
+				} else {
+					$status = "The team's chances are not looking good.";
+				}
+			} else {
+				if ($wins/($wins + $losses) >= .625) {
+					$status = "They are playoff bound!";
+				} else {
+					$status = "Oh well, there's always next year.";
+				}
+			}
+
+			$teamInfo = "After " . $totalGames . ", the " . $teamName . "have a record of" $record . ".";
+
+      return $teamInfo . "<br>" . $status;
+    }
 	}
